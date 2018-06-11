c23b519cf9426cba527e8c35deb1be255e75f615
Michael Hladky <michael@hladky.at>
2018-06-12 01:29:24 +0200

    setup root store

--- cli-workspace/projects/flight-app/src/app/app.module.ts
+++ cli-workspace/projects/flight-app/src/app/app.module.ts
@@ -13,6 +13,10 @@ import {HomeComponent} from './home/home.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {SharedModule} from './shared/shared.module';
 import {SidebarComponent} from './sidebar/sidebar.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './+state';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../../environments/environment';
 
 
 @NgModule({
@@ -23,7 +27,9 @@ import {SidebarComponent} from './sidebar/sidebar.component';
 
     FlightApiModule.forRoot(),
     SharedModule.forRoot(),
-    RouterModule.forRoot([...APP_ROUTES], {...APP_EXTRA_OPTIONS})
+    RouterModule.forRoot([...APP_ROUTES], {...APP_EXTRA_OPTIONS}),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   declarations: [
     AppComponent,
